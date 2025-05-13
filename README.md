4e9b68f3ebec044499d0e6eba419bba6faa524b8
Hyun Tae Kim <108547741+hyuntaekim1738@users.noreply.github.com>
2025-05-13 16:50:59 -0500

    navbar tweaks and pages added

--- README.md
+++ README.md
@@ -17,6 +17,6 @@ This is a **journaling app**. Intended features of MVP include:
 | **APIs** | Spotify webAPI  |
 ---
 
-## **Future ideas **
+## **Future ideas**
 - Integrating LLM to label and color code the mood of daily entries
 - Having a conversation mode: allowing users to make entries by having a conversation with the LLM and having the LLM either synthesize an entry from the conversation or directly save the conversation as an entry.
--- web-journal/app/components/NavBar.js
+++ web-journal/app/components/NavBar.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 export default function NavBar() {
   return (
-    <nav className="fixed top-0 left-0 w-full bg-gray-800 text-white py-6 z-50 border-b-2 border-b-neutral-100">
+    <nav className="fixed top-0 left-0 w-full text-white py-6 z-50 border-b-2 border-b-neutral-100">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-6 whitespace-nowrap">
 
         <div className="flex justify-between items-center lg:space-x-8">
@@ -13,25 +13,25 @@ export default function NavBar() {
 
           <div className="flex space-x-4 sm:space-x-2 lg:space-x-8">
             <Link
-              href="/projects"
+              href="/journal"
               className="sm:text-xs lg:text-lg hover:text-blue-500 transition duration-300"
             >
               Journal
             </Link>
             <Link
-              href="/experience"
+              href="/profile"
               className="sm:text-xs lg:text-lg hover:text-blue-500 transition duration-300"
             >
               Profile
             </Link>
             <Link
-              href="/experience"
+              href="/login"
               className="sm:text-xs lg:text-lg hover:text-blue-500 transition duration-300"
             >
               Log In
             </Link>
             <Link
-              href="/experience"
+              href="/signup"
               className="sm:text-xs lg:text-lg hover:text-blue-500 transition duration-300"
             >
               Sign Up
