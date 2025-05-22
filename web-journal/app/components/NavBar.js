ef59e8eafd4b0c343ae7fae974099ee51e192fd0
Hyun Tae Kim <108547741+hyuntaekim1738@users.noreply.github.com>
2025-05-22 17:35:33 -0500

    navbar personalization

--- web-journal/app/components/NavBar.js
+++ web-journal/app/components/NavBar.js
@@ -5,13 +5,14 @@ import { useSession, signOut } from 'next-auth/react';
 
 export default function NavBar() {
   const { data: session } = useSession();
+  const userFirstName = session?.user?.name?.split(' ')[0];
 
   return (
     <nav className="fixed top-0 left-0 w-full py-6 z-50 border-b-2 border-b-neutral-100">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-6 whitespace-nowrap">
         <div className="flex justify-between items-center lg:space-x-8">
           {session ? (<Link href="/home" className="sm:text-xl lg:text-2xl font-bold hover:text-blue-500 transition duration-150">
-              WebJournal
+            {userFirstName ? `${userFirstName}'s WebJournal` : 'My WebJournal'}
             </Link>): 
             (<Link href="/" className="sm:text-xl lg:text-2xl font-bold hover:text-blue-500 transition duration-150">
               WebJournal
