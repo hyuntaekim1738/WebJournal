ab8265336aa6ec08877669f751013bba893452b7
Hyun Tae Kim <108547741+hyuntaekim1738@users.noreply.github.com>
2025-05-23 20:07:09 -0500

    [5.5] - middleware update

--- web-journal/middleware.js
+++ web-journal/middleware.js
@@ -3,23 +3,28 @@ import { NextResponse } from 'next/server';
 
 export async function middleware(request) {
   const token = await getToken({ req: request });
-  const isAuthPage = request.nextUrl.pathname.startsWith('/login') || 
-                    request.nextUrl.pathname.startsWith('/signup');
-  const isHomePage = request.nextUrl.pathname === '/';
+  const { pathname } = request.nextUrl;
 
+  const isAuthPage = pathname.startsWith('/login') || pathname.startsWith('/signup');
+  const isHomePage = pathname === '/';
+
+  // redirect unauthenticated users to /login if they're not on /login, /signup, or /
   if (!token && !isAuthPage && !isHomePage) {
     return NextResponse.redirect(new URL('/login', request.url));
   }
-  if (token && isAuthPage) {
+
+  // redirect authenticated users away from /login, /signup, or / to /home
+  if (token && (isAuthPage || isHomePage)) {
     return NextResponse.redirect(new URL('/home', request.url));
   }
+
   return NextResponse.next();
 }
 
 export const config = {
   matcher: [
     /*
-     * mtach all request paths except for the ones starting with:
+     * Match all request paths except for the ones starting with:
      * - api (API routes)
      * - _next/static (static files)
      * - _next/image (image optimization files)
